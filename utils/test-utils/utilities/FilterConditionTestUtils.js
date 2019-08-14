f87536c79623d40ece726257395d3e9e5896fac4
Pascal ECHEMANN <pechemann@gmail.com>
2019-08-14 17:01:14 +0200

    Create FilterConditionTestUtils.js
    Add test suite for the FilterCondition enum.

---
+++ utils/test-utils/utilities/FilterConditionTestUtils.js
@@ -0,0 +1,7 @@
+"use strict";
+/*!
+ * This module constains utilities used by the FilterCondition test suite.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AND = 'AND';
+exports.OR = 'OR';
